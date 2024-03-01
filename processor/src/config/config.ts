b940841563f4f4bb39706d97ddabbf767b586e75
sfedynadev <sfedynadev@gmail.com>
2024-03-01 16:21:34 +0200

    Added new payment connector

--- processor/src/config/config.ts
+++ processor/src/config/config.ts
@@ -1,12 +1,12 @@
 export const config = {
   // Required by Payment SDK
-  projectKey: process.env.CTP_PROJECT_KEY || 'payment-integration',
-  clientId: process.env.CTP_CLIENT_ID || 'xxx',
-  clientSecret: process.env.CTP_CLIENT_SECRET || 'xxx',
-  jwksUrl: process.env.CTP_JWKS_URL || 'https://mc-api.europe-west1.gcp.commercetools.com/.well-known/jwks.json',
-  jwtIssuer: process.env.CTP_JWT_ISSUER || 'https://mc-api.europe-west1.gcp.commercetools.com',
-  authUrl: process.env.CTP_AUTH_URL || 'https://auth.europe-west1.gcp.commercetools.com',
-  apiUrl: process.env.CTP_API_URL || 'https://api.europe-west1.gcp.commercetools.com',
+  projectKey: process.env.CTP_PROJECT_KEY || 'checkout-project',
+  clientId: process.env.CTP_CLIENT_ID || 'iQZRe2833wyx-0XDoqZs-q55',
+  clientSecret: process.env.CTP_CLIENT_SECRET || 'wulPbiWsKTOwSLx0sdjPsqwQKfA28jVX',
+  jwksUrl: process.env.CTP_JWKS_URL || 'https://api.us-central1.gcp.commercetools.com/.well-known/jwks.json',
+  jwtIssuer: process.env.CTP_JWT_ISSUER || 'https://api.us-central1.gcp.commercetools.com',
+  authUrl: process.env.CTP_AUTH_URL || 'https://auth.us-central1.gcp.commercetools.com',
+  apiUrl: process.env.CTP_API_URL || 'https://api.us-central1.gcp.commercetools.com',
   sessionUrl: process.env.CTP_SESSION_URL || 'https://session.europe-west1.gcp.commercetools.com/',
   healthCheckTimeout: parseInt(process.env.HEALTH_CHECK_TIMEOUT || '5000'),
 
